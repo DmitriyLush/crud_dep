9cf42973664168ae7f381ac40f099a09557f5ca3
DmitriyLush <aorti0712@gmail.com>
2021-04-07 23:41:06 +0200

    initial

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-basedir = os.path.abspath(os.path.dirname(__file__))
+db = SQLAlchemy(app)
+
 
 # Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://zzfyhrcnyiczcp' \
@@ -12,19 +13,16 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://zzfyhrcnyiczcp' \
                                         '-69-72.eu-west-1.compute.amazonaws.com:5432/dd1oun8h39mb7n'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# Init db
-db = SQLAlchemy(app)
-
 
 # Storage class/Model
 class Storage(db.Model):
     __tablename__ = 'storage'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), unique=True)
-    amount = db.Column(db.Float)
+    amount = db.Column(db.Integer)
     measure = db.Column(db.String(10))
-    price = db.Column(db.Float)
-    cost = db.Column(db.Float)
+    price = db.Column(db.Integer)
+    cost = db.Column(db.Integer)
     date = db.Column(db.Date)
 
     def __init__(self, title, amount, measure, price, cost, date):
@@ -35,25 +33,20 @@ class Storage(db.Model):
         self.cost = cost
         self.date = date
 
-    def __repr__(self):
-        return f'Title {self.title}'
-
 
 @app.route('/resources', methods=['POST'])
 def input_data():
-    if request.content_type == 'application/json':
-        post_data = request.get_json()
-        title = post_data.get('title')
-        amount = post_data.get('amount')
-        measure = post_data.get('measure')
-        price = post_data.get('price')
-        cost = price * amount
-        date = datetime.strptime((post_data.get('date')), '%d-%m-%Y')
-        comm = Storage(title, amount, measure, price, cost, date)
-        db.session.add(comm)
-        db.session.commit()
-        return jsonify('Data posted')
-    return jsonify('Incorrect DATA')
+    post_data = request.get_json()
+    title = post_data.get('title')
+    amount = post_data.get('amount')
+    measure = post_data.get('measure')
+    price = post_data.get('price')
+    cost = price * amount
+    date = datetime.strptime((post_data.get('date')), '%d-%m-%Y')
+    comm = Storage(title, amount, measure, price, cost, date)
+    db.session.add(comm)
+    db.session.commit()
+    return jsonify('Data posted')
 
 
 @app.route('/resources', methods=['GET'])
@@ -67,41 +60,35 @@ def get_data():
     return jsonify({'resources': pre_res})
 
 
-@app.route('/resources/delete/<id>', methods=['DELETE'])
-def del_resource(id):
+@app.route('/resources', methods=['DELETE'])
+def del_data():
+    id = request.args.get('id')
     resource = db.session.query(Storage).get(id)
     db.session.delete(resource)
     db.session.commit()
     return jsonify('Deleted')
 
 
-@app.route('/resources/update/<id>', methods=['PUT'])
-def update_resource(id):
-    if request.content_type == 'application/json':
-        put_resource = request.get_json()
-        ID = put_resource.get('id')
-        amount = put_resource.get('amount')
-        price = put_resource.get('price')
-        title = put_resource.get('title')
-        rec = db.session.query(Storage).get(id)
-        rec.id = ID
-        rec.amount = amount
-        rec.price = price
-        rec.date = datetime.strptime((put_resource.get('date')), '%d-%m-%Y')
-        rec.title = title
-        rec.cost = rec.price * rec.amount
-        db.session.commit()
-        return jsonify('Success updated!')
+@app.route('/resources', methods=['PUT'])
+def update_data():
+    id = request.args.get('ID')
+    rec = db.session.query(Storage).get(id)
+    rec.id = request.args.get('ID')
+    rec.amount = request.args.get('amount')
+    rec.price = request.args.get('price')
+    rec.date = datetime.strptime((request.args.get('date')), '%d-%m-%Y')
+    rec.title = request.args.get('title')
+    rec.cost = int(rec.price) * int(rec.amount)
+    db.session.commit()
+    return jsonify('Successfully updated!')
 
 
 @app.route('/total_cost', methods=['GET'])
 def total_cost():
-    total_cost = 0
     resources = Storage.query.all()
-    for resource in resources:
-        total_cost += resource.cost
+    total_cost = sum([resource.cost for resource in resources])
     return jsonify({'total_cost': total_cost})
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
